7a601517892cf0d116c2860845e7838bacc3d28e
Yaxuan Huang <yaxuanh@umich.edu>
2025-11-11 22:51:20 -0500

    Export variable selection function

--- R/RandomLASSO_subgrad_Py.R
+++ R/RandomLASSO_subgrad_Py.R
@@ -1,8 +1,35 @@
-# This version allow the program penalize the intercept
-
-
+#' variable_selection_PY_penal_int
+#'
+#' This version allow the program penalize the intercept
+#'
+#' @param data the output of pesudo_outcomecal function
+#' @param ID the name of column where participants' ID are stored
+#' @param moderator_formula determines the formula for the f(St)T*beta function
+#' @param lam the value of penalty term of randomized LASSO. If it is not provided, the default value will be used
+#' @param noise_scale Scale of Gaussian noise added to objective. Default is \eqn{\sqrt{\frac{(1 - \rho)}{\rho}*number\ of\ rows}*sd(y)} where \eqn{\rho} is the split rate.
+#' The random noises, \eqn{\omega}, are iid drawn from normal distribution with standard deviation noise_scale
+#' @param splitrat this value is corresponding to the data splitting rate. Details can read "Exact Selective Inference with Randomization" page 15 equation (10).
+#' This value will be used only when user doesn't provide the noise_scale.
+#' @param virtualenv_path Python virtual environment path
+#'
+#' @return A table with the selected variables is returned. The returned table contains 
+#' Selected variables that remain in the model after penalization, Non-selected variables that were excluded from the model,
+#' Number of unique subjects in the dataset, Random noise added for each variables, Regularization parameter (lambda) used in the penalization, Subgradient of unselected variables, 
+#' OMEGA: (noise_scale)^2/4. Divide 4 because in theoretical calculation -1/2 is hidden inside penalty and random noise, sign_soln: Signs of the estimated coefficients for selected variable,
+#' Estimated coefficients of selected variables from the penalized regression, True beta projected on space of post-selection predictors, Boolean vector indicating which variables were selected.
+#' 
+#' @examples
+#'
+#' var_selection_python = variable_selection_PY_penal_int(data = pseudo_outcome_CVlasso,ID,
+#' my_formula, lam = NULL, noise_scale = NULL, splitrat = 0.8,
+#' virtualenv_path = "fakepath/wcls", ridge_term = 0, beta = NULL)
+#'
+#' @import reticulate
+#'
+#' @export
+#' 
 variable_selection_PY_penal_int = function(data,ID, moderator_formula, lam = NULL, noise_scale = NULL,
-                                 splitrat = 0.8, virtualenv_path, ridge_term = 0, beta = NULL) {
+                                           splitrat = 0.8, virtualenv_path, ridge_term = 0, beta = NULL) {
   # data: the output of pesudo_outcomecal function
   # ID: the name of column where participants' ID are stored
   # moderator_formula: determines the formula for the f(St)T*beta function
@@ -13,32 +40,32 @@ variable_selection_PY_penal_int = function(data,ID, moderator_formula, lam = NUL
   #           This value will be used only when user doesn't provide the noise_scale.
   # virtualenv_path: Python virtual environment path
   # ridge_term
-
+  
   ptSt = data[,"ptSt"]
   n = dplyr::n_distinct(data[,ID])
   wssqrt = c(sqrt(ptSt*(1-ptSt)*2/n^(1/2)))
-
+  
   X = data.matrix(modelr::model_matrix(data, moderator_formula))
   Xw = X * wssqrt # this gives the same result as diag(wssqrt) %*% X
   ystring <- toString(moderator_formula[[2]])
   val_names <- all.vars(moderator_formula)
   y <- data.matrix(dplyr::select(stats::na.omit(data[,val_names]),tidyselect::all_of(ystring)))
   yw = c(wssqrt*y)
-
+  
   #if(is.null(lam)) {lam = sqrt(2*log(dim(Xw)[2]))*sd(yw)*splitrat*sqrt(dim(Xw)[1])}
   mod = lm(yw ~ Xw)
   dispersion = sum(mod[["residuals"]]^2)/mod[["df.residual"]] # this value is slightly different from the python
   # because in python they use (Moore-Penrose) pseudo-inverse of a matrix
-
+  
   if(is.null(noise_scale)) {
     noise_scale = sqrt(dispersion * (1 - splitrat)/splitrat * dim(Xw)[1])
   }
   if(is.null(lam)) {lam = sqrt(2*log(dim(Xw)[2]))*sd(yw)*splitrat*sqrt(dim(Xw)[1])}
-
+  
   # load python virtualenv
   require(reticulate)
-  use_virtualenv(virtualenv_path)
-  #use_condaenv(condaenv = 'env3', conda = "/opt/anaconda3/bin/conda", required = TRUE)
+  # use_virtualenv(virtualenv_path)
+  use_condaenv(condaenv = 'env3', conda = "/opt/anaconda3/bin/conda", required = TRUE)
   # load required modules and functions
   np = import("numpy")
   selectinf = import("selectinf")
@@ -46,17 +73,17 @@ variable_selection_PY_penal_int = function(data,ID, moderator_formula, lam = NUL
   selected_targets = selectinf$base$selected_targets
   const = lassopy$gaussian
   exact_grid_inference = selectinf$randomized$exact_reference$exact_grid_inference
-
+  
   # convert data to Python array
   X1 = array_reshape(Xw, c(dim(Xw)[1], dim(Xw)[2]))
   Y1 = np_array(yw, dtype = "float64")
   # penalize the intercept
   wt = np_array(c(0,rep(lam,dim(Xw)[2]-1)))
-
-
+  
+  
   # plug in function
   conv = const(X1, Y1, feature_weights = wt, randomizer_scale = noise_scale, ridge_term = ridge_term)
-
+  
   # get sign solution
   signs = conv$fit()
   nonzero = (signs!=0)
@@ -65,11 +92,11 @@ variable_selection_PY_penal_int = function(data,ID, moderator_formula, lam = NUL
   soln = c(conv$observed_soln)
   subgrad = c(conv$observed_subgrad/lam)
   perturb = c(perturb$T)
-
+  
   E = colnames(X)[nonzero]
   NE = colnames(X)[!nonzero] # NE may include intercept
   Z = subgrad[!nonzero]
-
+  
   # calculate the post selection beta
   if(!is.null(beta) & sum(nonzero) > 1) {
     postbeta = np$dot(np$linalg$pinv(Xw[, nonzero]), np$dot(Xw, beta))
@@ -80,7 +107,7 @@ variable_selection_PY_penal_int = function(data,ID, moderator_formula, lam = NUL
   # But be careful if you change the weights, have to double check two calculation
   #wt = ptSt * (1-ptSt)
   #postbeta2 = solve(t(X[,nonzero] * c(wt / sqrt(n))) %*% X[,nonzero]) %*% t(X[,nonzero] * c(wt / sqrt(n))) %*% X %*% beta
-
+  
   # get all Python code return
   #disperson = np$linalg$norm(Y1 - np$dot(X1[,nonzero], np$dot(np$linalg$pinv(X1[,nonzero]), Y1)))^2 / (n - sum(nonzero))
   #conv$setup_inference(dispersion=dispersion)
@@ -92,7 +119,7 @@ variable_selection_PY_penal_int = function(data,ID, moderator_formula, lam = NUL
   #                        level=0.9)
   #G = exact_grid_inference(query_spec = conv$specification, target_spec = target_spec)
   #grids = as.matrix(G$stat_grid)
-
+  
   return(list(formula = moderator_formula, E = E, NE = NE, n = n,
               perturb = perturb/(-2), ori_lam = lam, lam = lam/(-2), Z = Z, OMEGA = (noise_scale)^2/4,
               sign_soln = signs[nonzero], soln = soln, postbeta = postbeta, nonzero = nonzero
@@ -103,8 +130,8 @@ variable_selection_PY_penal_int = function(data,ID, moderator_formula, lam = NUL
               #                     dispersion_point = np$dot(np$linalg$pinv(X1[,nonzero]), Y1),
               #                     HEE = t(X1[,nonzero]) %*% X1[,nonzero]/c(n),
               #                     grids = grids)
-              ))
-
+  ))
+  
   # Output:
   # E: Selected variables that remain in the model after penalization.
   # NE: Non-selected variables that were excluded from the model.
