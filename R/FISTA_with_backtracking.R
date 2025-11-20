69852421a1f1a1988fe63c8e7ce9131298e57990
Yaxuan Huang <yaxuanh@umich.edu>
2025-11-19 22:24:07 -0500

    Export FISTA_with_backtracking

--- R/FISTA_with_backtracking.R
+++ R/FISTA_with_backtracking.R
@@ -63,7 +63,8 @@
 #' FISTA_backtracking(data = sim_data, moderator_formula = my_formula, lam = NULL, noise_scale = NULL,
 #'   splitrat = 0.7, beta = matrix(c(-1, 1.7, 1.5, -1.3, -1, rep(0,21)), ncol = 1))
 #'
-#'
+#' @export
+#' 
 
 FISTA_backtracking = function(data,ID, moderator_formula, lam = NULL, noise_scale = NULL,
                               splitrat = 0.8, max_ite = 10^(5), tol = 10^(-4), beta = NULL){
